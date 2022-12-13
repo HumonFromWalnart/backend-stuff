9cec830d5d131a7e30fcf7f551c15a644c9e6a72
HumonFromWalnart <tlmuunulzii@gmail.com>
2022-12-13 20:11:16 +0800

    LOL

--- express.js
+++ express.js
@@ -1,23 +1,77 @@
 
 
-const express = require('express');
-
+import express, { response } from 'express';
 const port = 6942;
-
 const app = express();
 
-app.get("/", (request, response) =>{
-    response.send('Raus, Raus!');
+const users = [
+    { id: 1, name: 'Jonh' },
+    { id: 2, name: 'Male' },
+];
+
+app.get("/", (req, res, next) => {
+    res.status('200').json({
+        users: users,
+    });
 });
 
-app.get("/users", (request, response) => {
-    response.send([{Name :'Rick', Status : 'Never gonna give you up!'}]);
+app.get("/create", (req, res, next) => {
+    users.push({ id: users.length + 1, ...req.body });
+    res.status('400').json({ message: 'new user created' });
 });
 
-app.get("/post", (request, response) =>{
-    response.send([{Title : 'Rick Astley - Never Gonna Give You Up - [OFFICIAL MUSIC VIDEO]', Comment : 'Rick never make us give up!'}]);
+app.post("/:id", (req, res, next) => {
+    const { id } = req.params;
+    console.log(id)
+    if (id > users.length) {
+        res
+            .status('400')
+            .json({ message: `${req.params.id} id does not exist you dumb ass. ___       ________  ___          
+            |\  \     |\   __  \|\  \         
+            \ \  \    \ \  \|\  \ \  \        
+             \ \  \    \ \  \\\  \ \  \       
+              \ \  \____\ \  \\\  \ \  \____  
+               \ \_______\ \_______\ \_______\
+                \|_______|\|_______|\|_______|`})
+    } else {
+        res.status('200').json(users[parseInt(id) - 1]);
+    }
+});
+
+app.patch("/:id", (req, res, next) => {
+    const { id } = req.params;
+    if (id > users.length) {
+        res
+            .status('400')
+            .json({ message: `${req.params.id} id does not exist you dumb ass ___       ________  ___          
+            |\  \     |\   __  \|\  \         
+            \ \  \    \ \  \|\  \ \  \        
+             \ \  \    \ \  \\\  \ \  \       
+              \ \  \____\ \  \\\  \ \  \____  
+               \ \_______\ \_______\ \_______\
+                \|_______|\|_______|\|_______|` })
+    } else {
+        res.status('200').json({ message: `user with ${req.params.id} id is updated. Are you satisfied now?` })
+    }
+});
+
+app.delete("/:id", (req, res, next) => {
+    const { id } = req.params;
+    if (id > users.length) {
+        res
+            .status('400')
+            .json({ message: `${req.params.id} id does not exist you dumb ass ___       ________  ___          
+            |\  \     |\   __  \|\  \         
+            \ \  \    \ \  \|\  \ \  \        
+             \ \  \    \ \  \\\  \ \  \       
+              \ \  \____\ \  \\\  \ \  \____  
+               \ \_______\ \_______\ \_______\
+                \|_______|\|_______|\|_______|` })
+    } else {
+        res.status('200').json({ message: `user with ${req.params.id} id got killed ` })
+    }
 })
 
 app.listen(port, () => {
-    'Another successfull seizure!';
+    console.log(`${'Another successfull seizure!', port}`);
 });
